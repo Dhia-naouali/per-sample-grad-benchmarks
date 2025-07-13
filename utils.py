1b8f90d3e2bc1081e9843f70b464e62f113b19c6
Dhia Naouali <dhia12naouali@gmail.com>
2025-07-13 18:52:31 +0100

    added config loading utility

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import time
+import yaml
 import torch
-
 
 class Timer:
     def __enter__(self):
@@ -16,3 +16,20 @@ class Timer:
 def peak_memory():
     torch.cuda.reset_peak_memory_stats()
     return torch.cuda.max_memory_allocated() / 1024**2
+
+
+
+def load_config(path=None, kwargs):
+    with open(path) as f:
+        configs = yaml.safe_load(f)
+
+
+    for k in ["batches", "in_dim", "out_dim", "hiddens", "depths"]:
+        if k in kwargs and kwargs[k] is not None:
+            configs[k] = kwargs[k]
+
+    for k in ["batches", "hiddens", "depths"]:
+        if isinstance(configs[k], str):
+            configs[k] = [int(v) for v in configs[k].split(",")]
+
+    return configs
